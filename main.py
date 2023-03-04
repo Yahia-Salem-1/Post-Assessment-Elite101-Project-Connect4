1b192060f609d2ad0ef39df51783d65db5f78d25
Yahia Salem <121064542+Yahia-Salem-1@users.noreply.github.com>
2023-03-04 03:02:35 +0000

    Introduced isUsed set to true to make it an infinite while loop. Also made inputs for user selection and made the user select a row and a column, which displays on the Connect4 board.

--- main.py
+++ main.py
@@ -7,31 +7,98 @@ board = [
   ["   ","   ","   ","   ","   ","   "],
 ] # From https://replit.com/@AndrewFlye/Connect4-Board-Example#main.py
 
-
-rowA = board[0]
-rowB = board[1] # columns are part of the list of board
-rowC = board[2]
-rowD = board[3]
-rowE = board[4]
-rowF = board[5]
-row = rowA or rowB or rowC or rowD or rowE or rowF
-
-colA = row[0]  # columns are part of the list of row
-colB = row[1]
-colC = row[2]
-colD = row[3]
-colE = row[4]
-colF = row[5]
-col = colA or colB or colC or colD or colE or colF
+isUsed = True
 
 # Displays board in visually-pleasing way
 def display_board():
   for row in board: 
-    print("|{0}|{1}|{2}|{3}|{4}|{5}|".format(colA, colB, colC, colD, colE, colF))
+    print("|{0}|{1}|{2}|{3}|{4}|{5}|".format(*row)) # From https://www.geeksforgeeks.org/python-star-or-asterisk-operator/
 
- 
+def user_rowCol_selection():
+  print("\nEvery letter represents a row and column (only reaches F for each; rows then columns). Ex: AB. You CANNOT skip a row and must start from the bottom row. Choose whatever column you want.")
+  def user_rowCol_selection_x():
+    
+    row_selection = input("\nChoose Row (letter): ").upper()
+    col_selection = input("\nChoose Column (letter): ").upper()
 
+  
+    if row_selection == 'A':
+      selected_row = 0
+    elif row_selection == 'B':
+      selected_row = 1
+    elif row_selection == 'C':
+      selected_row = 2
+    elif row_selection == 'D':
+      selected_row = 3
+    elif row_selection == 'E':
+      selected_row = 4
+    elif row_selection == 'F':
+      selected_row = 5
+
+    if col_selection == 'A':
+      selected_col = 0
+      board[selected_row][selected_col] = ' x '
+    elif col_selection == 'B':
+      selected_col = 1
+      board[selected_row][selected_col] = ' x '
+    elif col_selection == 'C':
+      selected_col = 2
+      board[selected_row][selected_col] = ' x '
+    elif col_selection == 'D':
+      selected_col = 3
+      board[selected_row][selected_col] = ' x '
+    elif col_selection == 'E':
+      selected_col = 4
+      board[selected_row][selected_col] = ' x '
+    elif col_selection == 'F':
+      selected_col = 5
+      board[selected_row][selected_col] = ' x '
+
+  def user_rowCol_selection_o():
+    
+    row_selection = input("\nChoose Row (letter): ").upper()
+    col_selection = input("\nChoose Column (letter): ").upper()
+
+  
+    if row_selection == 'A':
+      selected_row = 0
+    elif row_selection == 'B':
+      selected_row = 1
+    elif row_selection == 'C':
+      selected_row = 2
+    elif row_selection == 'D':
+      selected_row = 3
+    elif row_selection == 'E':
+      selected_row = 4
+    elif row_selection == 'F':
+      selected_row = 5
+
+    if col_selection == 'A':
+      selected_col = 0
+      board[selected_row][selected_col] = ' o '
+    elif col_selection == 'B':
+      selected_col = 1
+      board[selected_row][selected_col] = ' o '
+    elif col_selection == 'C':
+      selected_col = 2
+      board[selected_row][selected_col] = ' o '
+    elif col_selection == 'D':
+      selected_col = 3
+      board[selected_row][selected_col] = ' o '
+    elif col_selection == 'E':
+      selected_col = 4
+      board[selected_row][selected_col] = ' o '
+    elif col_selection == 'F':
+      selected_col = 5
+      board[selected_row][selected_col] = ' o '
+
+
+  user_rowCol_selection_x()
+  user_rowCol_selection_o()
 def main(): #main function (where every function will go)
-  display_board()
+  while isUsed:
+    display_board()
+    user_rowCol_selection()
+  
 
 main()
